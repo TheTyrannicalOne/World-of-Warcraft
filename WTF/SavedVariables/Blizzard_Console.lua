36cdf4f869652f2c01c862f97c948550ce40e188
TheTyrannicalOne <TheTyrannicalOne@users.noreply.github.com>
2018-01-28 15:15:33 -0500

    Auto-Commit, profile switching

--- WTF/SavedVariables/Blizzard_Console.lua
+++ WTF/SavedVariables/Blizzard_Console.lua
@@ -1,15 +1,211 @@
 
 Blizzard_Console_SavedVars = {
 	["version"] = 3,
-	["height"] = 300,
 	["messageHistory"] = {
 		{
 			"-------------------------------------------------- Previous Session --------------------------------------------------", -- [1]
 			0, -- [2]
 		}, -- [1]
+		{
+			"-------------------------------------------------- Previous Session --------------------------------------------------", -- [1]
+			0, -- [2]
+		}, -- [2]
+		{
+			"Sorting particles normally.", -- [1]
+			0, -- [2]
+		}, -- [3]
+		{
+			"Detail doodad instancing enabled.", -- [1]
+			0, -- [2]
+		}, -- [4]
+		{
+			"Water detail changed to 2", -- [1]
+			0, -- [2]
+		}, -- [5]
+		{
+			"Ripple detail changed to 1", -- [1]
+			0, -- [2]
+		}, -- [6]
+		{
+			"Reflection mode changed to 0", -- [1]
+			0, -- [2]
+		}, -- [7]
+		{
+			"Reflection downscale changed to 0", -- [1]
+			0, -- [2]
+		}, -- [8]
+		{
+			"Sunshafts quality changed to 2", -- [1]
+			0, -- [2]
+		}, -- [9]
+		{
+			"Refraction mode changed to 2", -- [1]
+			0, -- [2]
+		}, -- [10]
+		{
+			"Enabling BSP node cache (first time - starting up)", -- [1]
+			0, -- [2]
+		}, -- [11]
+		{
+			"WorldPoolUsage must be stream, dynamic or static.", -- [1]
+			0, -- [2]
+		}, -- [12]
+		{
+			"CVar 'worldPoolUsage' failed validation for its initial value.", -- [1]
+			0, -- [2]
+		}, -- [13]
+		{
+			"Alpha map bit depth set to 8bit on restart.", -- [1]
+			0, -- [2]
+		}, -- [14]
+		{
+			"Hardware PCF enabled.", -- [1]
+			0, -- [2]
+		}, -- [15]
+		{
+			"Projected textures enabled.", -- [1]
+			0, -- [2]
+		}, -- [16]
+		{
+			"Texture cache size set to 512MB.", -- [1]
+			0, -- [2]
+		}, -- [17]
+		{
+			"Shadow mode changed to 3 - 3 band dynamic shadows, 2048", -- [1]
+			0, -- [2]
+		}, -- [18]
+		{
+			"Shadow texture size changed to 2048.", -- [1]
+			0, -- [2]
+		}, -- [19]
+		{
+			"Soft shadows changed to 0.", -- [1]
+			0, -- [2]
+		}, -- [20]
+		{
+			"SSAO mode set to 3", -- [1]
+			0, -- [2]
+		}, -- [21]
+		{
+			"SSAO distance value set to 100.000000", -- [1]
+			0, -- [2]
+		}, -- [22]
+		{
+			"SSAO blur set to 2", -- [1]
+			0, -- [2]
+		}, -- [23]
+		{
+			"Depth Based Opacity Enabled", -- [1]
+			0, -- [2]
+		}, -- [24]
+		{
+			"SkyCloudLOD set to 0", -- [1]
+			0, -- [2]
+		}, -- [25]
+		{
+			"Terrain mip level changed to 0.", -- [1]
+			0, -- [2]
+		}, -- [26]
+		{
+			"Outline mode changed to 2", -- [1]
+			0, -- [2]
+		}, -- [27]
+		{
+			"LightBuffer mode changed to 2", -- [1]
+			0, -- [2]
+		}, -- [28]
+		{
+			"Physics interaction level changed to 1", -- [1]
+			0, -- [2]
+		}, -- [29]
+		{
+			"Render scale changed to 1", -- [1]
+			0, -- [2]
+		}, -- [30]
+		{
+			"Resample quality changed to 0", -- [1]
+			0, -- [2]
+		}, -- [31]
+		{
+			"MSAA disabled", -- [1]
+			0, -- [2]
+		}, -- [32]
+		{
+			"MSAA for alpha-test enabled.", -- [1]
+			0, -- [2]
+		}, -- [33]
+		{
+			"Component texture lod changed to 0", -- [1]
+			0, -- [2]
+		}, -- [34]
+		{
+			"Component texture lod changed to 0", -- [1]
+			0, -- [2]
+		}, -- [35]
+		{
+			"World preload object sort enabled.", -- [1]
+			0, -- [2]
+		}, -- [36]
+		{
+			"World load object sort enabled.", -- [1]
+			0, -- [2]
+		}, -- [37]
+		{
+			"World preload non critical enabled.", -- [1]
+			0, -- [2]
+		}, -- [38]
+		{
+			"World preload high res textures enabled.", -- [1]
+			0, -- [2]
+		}, -- [39]
+		{
+			"Full memory crash dump disabled", -- [1]
+			0, -- [2]
+		}, -- [40]
+		{
+			"Error display disabled", -- [1]
+			0, -- [2]
+		}, -- [41]
+		{
+			"Error display shown", -- [1]
+			0, -- [2]
+		}, -- [42]
+		{
+			"Displaying errors through fatal errors", -- [1]
+			0, -- [2]
+		}, -- [43]
+		{
+			"Displaying errors through fatal errors", -- [1]
+			0, -- [2]
+		}, -- [44]
+		{
+			"Now filtering: all messages", -- [1]
+			0, -- [2]
+		}, -- [45]
+		{
+			"CVar 'Sound_AmbienceHighpassDSPCutoff' failed validation for its initial value.", -- [1]
+			0, -- [2]
+		}, -- [46]
+		{
+			"CVar 'Sound_OtherPlayerHighpassDSPCutoff' failed validation for its initial value.", -- [1]
+			0, -- [2]
+		}, -- [47]
+		{
+			"CVar 'Sound_NPCHighpassDSPCutoff' failed validation for its initial value.", -- [1]
+			0, -- [2]
+		}, -- [48]
+		{
+			"FFX: Anti Aliasing Mode disabled", -- [1]
+			0, -- [2]
+		}, -- [49]
+		{
+			"FFX: Color Blind Test Mode Disabled", -- [1]
+			0, -- [2]
+		}, -- [50]
 	},
-	["isShown"] = false,
+	["height"] = 300,
 	["fontHeight"] = 14,
+	["isShown"] = false,
 	["commandHistory"] = {
 	},
 }
